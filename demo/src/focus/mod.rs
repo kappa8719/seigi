a507dd1d35a65fd14684c4d9b944adbf64a49930
kappa <kappa@entropi.kr>
2025-10-10 08:28:34 +0900

    chore(demo): add demo for focus traps

---
+++ demo/src/focus/mod.rs
@@ -0,0 +1,37 @@
+use gloo::{events::EventListener, utils::document};
+use seigi::focus::FocusTrapOptions;
+use wasm_bindgen::JsCast;
+
+use crate::escape_selector;
+
+fn initialize_trap(activate_selector: &str, target_selector: &str) {
+    let activate_selector = escape_selector(activate_selector);
+    let target_selector = escape_selector(target_selector);
+
+    let activate = document()
+        .query_selector(activate_selector.as_str())
+        .unwrap()
+        .unwrap();
+    let target = document()
+        .query_selector(target_selector.as_str())
+        .unwrap()
+        .unwrap();
+
+    let trap = seigi::focus::create_focus_trap(FocusTrapOptions::new(target.clone()));
+
+    EventListener::new(activate.clone().unchecked_ref(), "click", move |event| {
+        if trap.is_activated() {
+            trap.deactivate();
+            target.remove_attribute("data-seigi-trap-active");
+        } else {
+            trap.activate();
+            target.set_attribute("data-seigi-trap-active", "");
+        }
+    })
+    .forget();
+}
+
+pub fn initialize() {
+    initialize_trap("#focus.trap.default.activate", "#focus.trap.default");
+    initialize_trap("#focus.trap.esc.activate", "#focus.trap.esc");
+}
--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,6 +1,7 @@
 use gloo::console::console_dbg;
 use seigi::toast::ToasterOptions;
 
+mod focus;
 mod router;
 mod toast;
 
@@ -8,5 +9,11 @@ fn main() {
     console_error_panic_hook::set_once();
     router::initialize();
     seigi::toast::initialize(ToasterOptions::default());
+
     toast::initialize();
+    focus::initialize();
+}
+
+fn escape_selector(selector: &str) -> String {
+    selector.replace(".", "\\.")
 }
